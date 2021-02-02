d318d8b9071821b5d79a1e99c53976ac981ca8df
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-02 09:17:25 +0100

    fixed bug of non finding of file

--- assignment2/src/hangman/Main.java
+++ assignment2/src/hangman/Main.java
@@ -2,7 +2,7 @@ package hangman;
 
 public class Main {
 	public static void main(String[] args) {
-		WordReader file = new WordReader("words.txt");
+		WordReader file = new WordReader("assignment2/words.txt");
 		System.out.println(file.getWord());
 	}
 }
